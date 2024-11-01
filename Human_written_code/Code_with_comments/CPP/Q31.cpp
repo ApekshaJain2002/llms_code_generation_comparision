7b6cd9dc2c7d1720732a63798c343f94e7b4b3ca
avishagoyal08 <90690345+avishagoyal08@users.noreply.github.com>
2024-11-01 21:57:17 +0530

    Update Q31.cpp

--- Human_written_code/Code_with_comments/CPP/Q31.cpp
+++ Human_written_code/Code_with_comments/CPP/Q31.cpp
@@ -1,49 +1,17 @@
 #include <iostream>
-#include <map>
 using namespace std;
- 
 int main()
 {
     int t;
-    cin >> t;
-    
-    while (t--)
+    cin>>t;
+    while(t--)
     {
-        map<char, int> m;
-        string n;
-        cin >> n;
-        int ans = 0;
-        for (int i = 0; i < n.length(); i++)
-        {
-            m[n[i]]++;
-        }
- 
-        // for (auto it : m)
-        //     cout << it.first << " " << it.second << endl;
- 
-        if (m.size() == 4)
-            cout << 4 << endl;
-        else if (m.size() == 1)
-            cout << -1 << endl;
-        else if (m.size() == 3)
-        {
-            cout << 4 << endl;
-        }
-        else
-        {
-            for (auto it : m)
-            {
-                if (it.second == 3)
-                {
-                    cout << 6 << endl;
-                    ans = 1;
-                    break;
-                }
-            }
-            if (ans == 0)
- 
-                cout << 4 << endl;
-        }
+    int a,b;
+    cin>>a>>b;
+    if((a+b) % 2 == 0)
+    cout<<"Bob"<<endl;
+    else
+    cout<<"Alice"<<endl;
     }
  
     return 0;
