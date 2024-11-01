c09d444ab06795ea7990bbce52d41e61782364fb
avishagoyal08 <90690345+avishagoyal08@users.noreply.github.com>
2024-11-01 14:34:11 +0530

    Create Q29.cpp

---
+++ Human_written_code/Code_with_comments/CPP/Q29.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        int ct=0;
+        sort(arr,arr+n);
+        for(int i=0;i<n-1;i++)
+        {
+            if(arr[i]==arr[i+1])
+            {
+                cout<<"NO"<<endl;
+                break;
+            }
+            else
+            ct++;
+        }
+        if(ct==n-1)
+        cout<<"YES"<<endl;
+    }
+ 
+    return 0;
+}
