55e8c0b6ff40917393f79261ff8999a461242ca6
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 12:51:47 +0530

    Update Q30.cpp

--- Human_written_code/Code_with_comments/CPP/Q30.cpp
+++ Human_written_code/Code_with_comments/CPP/Q30.cpp
@@ -1,50 +1,42 @@
 #include <iostream>
 #include <map>
 using namespace std;
- 
-int main()
-{
+
+int main() {
     int t;
-    cin >> t;
-    
-    while (t--)
-    {
+    cin >> t;  // Read number of test cases
+
+    while (t--) {
         map<char, int> m;
         string n;
-        cin >> n;
+        cin >> n;  // Read input string
         int ans = 0;
-        for (int i = 0; i < n.length(); i++)
-        {
+
+        // Count frequency of each character in the string
+        for (int i = 0; i < n.length(); i++) {
             m[n[i]]++;
         }
- 
-        // for (auto it : m)
-        //     cout << it.first << " " << it.second << endl;
- 
+
+        // Check cases based on the number of unique characters
         if (m.size() == 4)
-            cout << 4 << endl;
+            cout << 4 << endl;  // All characters are unique
         else if (m.size() == 1)
-            cout << -1 << endl;
+            cout << -1 << endl;  // All characters are the same
         else if (m.size() == 3)
-        {
-            cout << 4 << endl;
-        }
-        else
-        {
-            for (auto it : m)
-            {
-                if (it.second == 3)
-                {
+            cout << 4 << endl;  // Three unique characters
+        else {
+            // When there are two unique characters, check their counts
+            for (auto it : m) {
+                if (it.second == 3) {  // One character appears three times
                     cout << 6 << endl;
                     ans = 1;
                     break;
                 }
             }
             if (ans == 0)
- 
-                cout << 4 << endl;
+                cout << 4 << endl;  // Otherwise, both characters appear twice
         }
     }
- 
+
     return 0;
 }
