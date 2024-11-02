87135f8e7e09bf0fad7e32cc0b661706cc174a8f
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 12:38:21 +0530

    Update Q22.cpp

--- Human_written_code/Code_with_comments/CPP/Q22.cpp
+++ Human_written_code/Code_with_comments/CPP/Q22.cpp
@@ -3,22 +3,23 @@ using namespace std;
 
 void solve() {
     long long a, b, c, d;
-    cin >> a >> b >> c >> d;
-    long long x = a * d, y = b * c;
-    if (x == y)
-        cout << "0\n";
+    cin >> a >> b >> c >> d;  // Read four integers
+    long long x = a * d, y = b * c;  // Compute cross-products
+
+    if (x == y)  // Check if products are equal
+        cout << "0\n";  // Output 0 if ratios are equal
     else if ((y != 0 && x % y == 0) || (x != 0 && y % x == 0))
-        cout << "1\n";
+        cout << "1\n";  // Output 1 if one ratio is a multiple of the other
     else
-        cout << "2\n";
+        cout << "2\n";  // Output 2 if ratios are neither equal nor multiples
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);  // Optimize input/output speed
+    cin.tie(nullptr);  // Untie cin and cout for faster IO
 
     int t;
-    cin >> t;
+    cin >> t;  // Read number of test cases
     while (t--)
-        solve();
+        solve();  // Solve each test case
 }
