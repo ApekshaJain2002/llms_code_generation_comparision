9e88468826046104b81ddeb03e53a32e69b740b3
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 12:50:42 +0530

    Update Q29.cpp

--- Human_written_code/Code_with_comments/CPP/Q29.cpp
+++ Human_written_code/Code_with_comments/CPP/Q29.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
-int main()
-{
-    int t,n;
-    cin>>t;
-    while(t--)
-    {
-        cin>>n;
+
+int main() {
+    int t, n;
+    cin >> t;  // Read the number of test cases
+
+    while (t--) {
+        cin >> n;
         int arr[n];
-        for(int i=0;i<n;i++)
-        {
-            cin>>arr[i];
+        
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];  // Read array elements
         }
-        int ct=0;
-        sort(arr,arr+n);
-        for(int i=0;i<n-1;i++)
-        {
-            if(arr[i]==arr[i+1])
-            {
-                cout<<"NO"<<endl;
+
+        int ct = 0;
+        sort(arr, arr + n);  // Sort the array to check for duplicates
+
+        // Check if there are any consecutive duplicate elements
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {  // If duplicate found, output "NO" and break
+                cout << "NO" << endl;
                 break;
+            } else {
+                ct++;
             }
-            else
-            ct++;
         }
-        if(ct==n-1)
-        cout<<"YES"<<endl;
+
+        // If all elements are unique, output "YES"
+        if (ct == n - 1)
+            cout << "YES" << endl;
     }
- 
+
     return 0;
 }
