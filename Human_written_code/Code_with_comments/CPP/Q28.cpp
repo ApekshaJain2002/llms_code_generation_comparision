812c2944f8c17f0b78e9e070f386ad79ec2bcc88
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 12:49:08 +0530

    Update Q28.cpp

--- Human_written_code/Code_with_comments/CPP/Q28.cpp
+++ Human_written_code/Code_with_comments/CPP/Q28.cpp
@@ -1,42 +1,35 @@
 #include <iostream>
- 
 using namespace std;
- 
-int main()
-{
+
+int main() {
     string s;
-    cin>>s;
-    int upper=0,lower=0;
-    int l= s.length();
-    for(int i=0;i<l;i++)
-    {
-        if(s[i]>='A' && s[i]<='Z')
-        upper++;
-        else if(s[i]>='a' && s[i]<='z')
-        lower++;
-        
+    cin >> s;
+    int upper = 0, lower = 0;
+    int l = s.length();
+
+    // Count uppercase and lowercase letters
+    for (int i = 0; i < l; i++) {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+            upper++;
+        else if (s[i] >= 'a' && s[i] <= 'z')
+            lower++;
     }
-    if(lower>=upper)
-        {
-            for(int i=0;i<l;i++)
-            {
-                if(s[i]>='A' && s[i]<='Z')
-                {
-                    s[i]=s[i]+32;
-                }
+
+    // Convert the string based on the count comparison
+    if (lower >= upper) {
+        for (int i = 0; i < l; i++) {
+            if (s[i] >= 'A' && s[i] <= 'Z') {
+                s[i] = s[i] + 32;  // Convert to lowercase
             }
         }
-        else
-        {
-            for(int i=0;i<l;i++)
-            {
-                if(s[i]>='a' && s[i]<='z')
-                {
-                    s[i]=s[i]-32;
-                }
+    } else {
+        for (int i = 0; i < l; i++) {
+            if (s[i] >= 'a' && s[i] <= 'z') {
+                s[i] = s[i] - 32;  // Convert to uppercase
             }
         }
-        cout<<s<<endl;
- 
+    }
+
+    cout << s << endl;
     return 0;
 }
