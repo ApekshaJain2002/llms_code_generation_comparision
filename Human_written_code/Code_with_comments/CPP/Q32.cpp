6567c256d32a1906ed6572b4e2da6bad3d4a8b53
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 12:54:52 +0530

    Update Q32.cpp

--- Human_written_code/Code_with_comments/CPP/Q32.cpp
+++ Human_written_code/Code_with_comments/CPP/Q32.cpp
@@ -1,42 +1,39 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+
+int main() {
     int t;
-    cin >> t;
-    while (t--)
-    {
+    cin >> t;  // Read the number of test cases
+
+    while (t--) {
         int n;
-        cin >> n;
-        string str = "meow";
-        string str1 = "MEOW";
+        cin >> n;  // Read the length of the string
+        string str = "meow", str1 = "MEOW";
         string s;
-        cin >> s;
+        cin >> s;  // Read the input string
         int cnt = 0;
-        if ((s[0] != 'm' && s[0] != 'M') || (s[n - 1] != 'w' && s[n - 1] != 'W'))
-        {
+
+        // Check if the string starts with 'm/M' and ends with 'w/W'
+        if ((s[0] != 'm' && s[0] != 'M') || (s[n - 1] != 'w' && s[n - 1] != 'W')) {
             cnt = -1;
-        }
-        else
-        {
+        } else {
             int j = 0;
-            for (int i = 1; i < n; i++)
-            {
-                if (s[i] != str[j] && s[i] != str1[j])
-                {
-                    j++;
+
+            // Iterate through the string to check for 'meow' pattern
+            for (int i = 1; i < n; i++) {
+                if (s[i] != str[j] && s[i] != str1[j]) {
+                    j++;  // Move to the next character in 'meow'
                 }
- 
-                if (s[i] == str[j] || s[i] == str1[j])
-                {
+
+                if (s[i] == str[j] || s[i] == str1[j]) {
                     continue;
-                }
-                else
-                {
-                    cnt = -1;
+                } else {
+                    cnt = -1;  // If pattern breaks, mark as invalid
                 }
             }
         }
+
+        // Output result based on validity of pattern
         if (cnt == -1)
             cout << "NO" << endl;
         else
