cb1f4555bea0a5beccad9ee4e654e6b84866691f
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 13:27:55 +0530

    Update Q22.cpp

--- Human_written_code/Code_without_comments/CPP/Q22.cpp
+++ Human_written_code/Code_without_comments/CPP/Q22.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+    long long a, b, c, d;
+    cin >> a >> b >> c >> d;
+    long long x = a * d, y = b * c;
+
+    if (x == y)
+        cout << "0\n";
+    else if ((y != 0 && x % y == 0) || (x != 0 && y % x == 0))
+        cout << "1\n";
+    else
+        cout << "2\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
 
