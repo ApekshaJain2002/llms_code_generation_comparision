60989f848a297b3e9ca8af7445e1ec2123b11227
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 12:44:57 +0530

    Update Q24.cpp

--- Human_written_code/Code_with_comments/CPP/Q24.cpp
+++ Human_written_code/Code_with_comments/CPP/Q24.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 #include <string>
- 
+
 using namespace std;
- 
-int main()
-{
+
+int main() {
     string s;
-    int d = 1;
-    
+    int d = 1;  // Counter for consecutive identical characters
+
     cin >> s;
- 
-    for (int i = 0; i < s.length() - 1; ++i)
-    {
-        if(d >= 7)
+
+    for (int i = 0; i < s.length() - 1; ++i) {
+        if (d >= 7)  // Stop if 7 consecutive identical characters are found
             break;
- 
-        if(s[i] == s[i+1]) {
-            d++;
-        }else{
-            d = 1;
+
+        if (s[i] == s[i + 1]) {
+            d++;  // Increment count if characters match
+        } else {
+            d = 1;  // Reset count if characters differ
         }
     }
- 
-    if(d >= 7)
-        cout << "YES";
-    else
-        cout << "NO";
- 
+
+    // Output result based on the count of consecutive identical characters
+    cout << (d >= 7 ? "YES" : "NO");
+
     return 0;
- 
 }
