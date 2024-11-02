c173710c1d9112c23ce4c127d24d5e10a0ca0f39
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 13:55:29 +0530

    Update Q34.cpp

--- Human_written_code/Code_without_comments/CPP/Q34.cpp
+++ Human_written_code/Code_without_comments/CPP/Q34.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maximumProfit(int n, int fee, vector<int>& Arr) {
+    if (n == 0) return 0;
+
+    vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+    for (int ind = n - 1; ind >= 0; ind--) {
+        for (int buy = 0; buy <= 1; buy++) {
+            int profit;
+
+            if (buy == 0) {
+                profit = max(0 + dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1]);
+            }
+
+            if (buy == 1) {
+                profit = max(0 + dp[ind + 1][1], Arr[ind] - fee + dp[ind + 1][0]);
+            }
+
+            dp[ind][buy] = profit;
+        }
+    }
+
+    return dp[0][0];
+}
+
+int main() {
+    vector<int> prices = {1, 3, 2, 8, 4, 9};
+    int n = prices.size();
+    int fee = 2;
+
+    cout << "The maximum profit that can be generated is " << maximumProfit(n, fee, prices) << endl;
+    return 0;
+}
 
