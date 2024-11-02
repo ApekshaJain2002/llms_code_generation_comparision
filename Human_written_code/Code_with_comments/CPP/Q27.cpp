6ccdcf27e9870a3cb4e08b5fafac59bc94d09943
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 12:47:10 +0530

    Update Q27.cpp

--- Human_written_code/Code_with_comments/CPP/Q27.cpp
+++ Human_written_code/Code_with_comments/CPP/Q27.cpp
@@ -1,28 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
- 
-int main()
-{
+
+int main() {
     int n;
-    cin>>n;
-    int x[n],y[n],z[n];
-    for(int i=0;i<n;i++)
-    {
-        
-        cin>>x[i]>>y[i]>>z[i];
+    cin >> n;  // Read the number of force vectors
+    int x[n], y[n], z[n];
+
+    // Read the components of each vector
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i] >> z[i];
     }
-    
-    int sum1=0,sum2=0,sum3=0;
-    for(int i=0;i<n;i++)
-    {
-        sum1= sum1+x[i];
-        sum2=sum2+y[i];
-        sum3=sum3+z[i];
+
+    int sum1 = 0, sum2 = 0, sum3 = 0;
+
+    // Calculate the sum of components in each direction
+    for (int i = 0; i < n; i++) {
+        sum1 += x[i];
+        sum2 += y[i];
+        sum3 += z[i];
     }
-    //cout<<sum1<<" "<<sum2<<" "<<sum3<<endl;
-    if(sum1==0 && sum2==0 && sum3==0)
-    cout<<"YES"<<endl;
+
+    // Check if the vector sum is zero in all directions
+    if (sum1 == 0 && sum2 == 0 && sum3 == 0)
+        cout << "YES" << endl;  // The forces are in equilibrium
     else
-    cout<<"NO"<<endl;
+        cout << "NO" << endl;   // The forces are not in equilibrium
+
     return 0;
 }
