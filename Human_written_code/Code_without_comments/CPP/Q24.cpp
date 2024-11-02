ca9b168b5e88d625db976845d2f1240d1b89eaec
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 13:29:13 +0530

    Update Q24.cpp

--- Human_written_code/Code_without_comments/CPP/Q24.cpp
+++ Human_written_code/Code_without_comments/CPP/Q24.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    int d = 1;
+
+    cin >> s;
+
+    for (int i = 0; i < s.length() - 1; ++i) {
+        if (d >= 7)
+            break;
+
+        if (s[i] == s[i + 1]) {
+            d++;
+        } else {
+            d = 1;
+        }
+    }
+
+    cout << (d >= 7 ? "YES" : "NO");
+
+    return 0;
+}
 
