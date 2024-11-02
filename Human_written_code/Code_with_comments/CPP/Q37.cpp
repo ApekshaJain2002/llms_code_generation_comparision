3b26667c8c5f6d15f68c7dc254acef55d14d5bc2
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 12:55:51 +0530

    Update Q37.cpp

--- Human_written_code/Code_with_comments/CPP/Q37.cpp
+++ Human_written_code/Code_with_comments/CPP/Q37.cpp
@@ -1,24 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+// Function to check if a string has valid balanced parentheses
 bool isValid(string s) {
-        stack<char>st; 
-        for(auto it: s) {
-            if(it=='(' || it=='{' || it == '[') st.push(it); 
-            else {
-                if(st.size() == 0) return false; 
-                char ch = st.top(); 
-                st.pop(); 
-                if((it == ')' and ch == '(') or  (it == ']' and ch == '[') or (it == '}' and ch == '{')) continue;
-                else return false;
-            }
+    stack<char> st;
+
+    // Iterate through each character in the string
+    for (auto it : s) {
+        // Push opening brackets onto the stack
+        if (it == '(' || it == '{' || it == '[') 
+            st.push(it);
+        else {
+            // If stack is empty and a closing bracket is encountered, it's invalid
+            if (st.empty()) return false;
+
+            char ch = st.top();
+            st.pop();
+
+            // Check if the closing bracket matches the top opening bracket
+            if ((it == ')' && ch == '(') || (it == ']' && ch == '[') || (it == '}' && ch == '{'))
+                continue;
+            else
+                return false;
         }
-        return st.empty(); 
     }
-int main()
-{
-    string s="()[{}()]";
-    if(isValid(s))
-    cout<<"True"<<endl;
+
+    // If the stack is empty, all brackets are matched; otherwise, it's invalid
+    return st.empty();
+}
+
+int main() {
+    string s = "()[{}()]";
+
+    // Output result based on the validity of the string
+    if (isValid(s))
+        cout << "True" << endl;
     else
-    cout<<"False"<<endl;
+        cout << "False" << endl;
 }
