5e6781cd1748087ab6b822c9700b40a066536828
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 13:52:20 +0530

    Update Q32.cpp

--- Human_written_code/Code_without_comments/CPP/Q32.cpp
+++ Human_written_code/Code_without_comments/CPP/Q32.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        string str = "meow", str1 = "MEOW";
+        string s;
+        cin >> s;
+        int cnt = 0;
+
+        if ((s[0] != 'm' && s[0] != 'M') || (s[n - 1] != 'w' && s[n - 1] != 'W')) {
+            cnt = -1;
+        } else {
+            int j = 0;
+
+            for (int i = 1; i < n; i++) {
+                if (s[i] != str[j] && s[i] != str1[j]) {
+                    j++;
+                }
+
+                if (s[i] == str[j] || s[i] == str1[j]) {
+                    continue;
+                } else {
+                    cnt = -1;
+                }
+            }
+        }
+
+        if (cnt == -1)
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
+    }
+}
 
