a1889e27a5f96fb3ab2d505a70b9a72812960907
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 13:38:11 +0530

    Update Q27.cpp

--- Human_written_code/Code_without_comments/CPP/Q27.cpp
+++ Human_written_code/Code_without_comments/CPP/Q27.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int x[n], y[n], z[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i] >> z[i];
+    }
+
+    int sum1 = 0, sum2 = 0, sum3 = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum1 += x[i];
+        sum2 += y[i];
+        sum3 += z[i];
+    }
+
+    if (sum1 == 0 && sum2 == 0 && sum3 == 0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
 
