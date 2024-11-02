cfc24c08d2c25a46d7773f686441fd31984872e0
avishagoyal08 <90690345+avishagoyal08@users.noreply.github.com>
2024-11-02 13:25:54 +0530

    Create Q35.py

---
+++ Human_written_code/Code_without_comments/Python/Q35.py
@@ -0,0 +1,16 @@
+def longest_increasing_subsequence_length(arr):
+    n = len(arr)
+    temp = [arr[0]]
+    length = 1
+    for i in range(1, n):
+        if arr[i] > temp[-1]:
+            temp.append(arr[i])
+            length += 1
+        else:
+            ind = bisect_left(temp, arr[i])
+            temp[ind] = arr[i]
+    return length
+if __name__ == "__main__":
+    arr = [10, 9, 2, 5, 3, 7, 101, 18]
+    result = longest_increasing_subsequence_length(arr)
+    print("The length of the longest increasing subsequence is", result)
