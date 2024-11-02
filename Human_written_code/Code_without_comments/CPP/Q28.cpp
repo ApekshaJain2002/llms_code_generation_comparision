bffa54f4a999812bc84be2ac8593d2bf08169596
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-02 13:39:25 +0530

    Update Q28.cpp

--- Human_written_code/Code_without_comments/CPP/Q28.cpp
+++ Human_written_code/Code_without_comments/CPP/Q28.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int upper = 0, lower = 0;
+    int l = s.length();
+
+    for (int i = 0; i < l; i++) {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+            upper++;
+        else if (s[i] >= 'a' && s[i] <= 'z')
+            lower++;
+    }
+
+    if (lower >= upper) {
+        for (int i = 0; i < l; i++) {
+            if (s[i] >= 'A' && s[i] <= 'Z') {
+                s[i] = s[i] + 32;
+            }
+        }
+    } else {
+        for (int i = 0; i < l; i++) {
+            if (s[i] >= 'a' && s[i] <= 'z') {
+                s[i] = s[i] - 32;
+            }
+        }
+    }
+
+    cout << s << endl;
+    return 0;
+}
 
