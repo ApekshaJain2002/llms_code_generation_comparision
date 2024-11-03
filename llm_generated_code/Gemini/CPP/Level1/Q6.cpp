ef403a3ed208eb43f16776a6915727091704fa13
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-03 17:24:20 +0530

    Update Q6.cpp

--- llm_generated_code/Gemini/CPP/Level1/Q6.cpp
+++ llm_generated_code/Gemini/CPP/Level1/Q6.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+#include <sstream>
 
+using namespace std;
+
+string reverseWords(string s) {
+    stringstream ss(s);
+    string word, reversed;
+
+    while (ss >> word) {
+        reverse(word.begin(), word.end());
+        reversed += word + " ";
+    }
+
+    reversed.pop_back(); // Remove the extra space at the end
+
+    return reversed;
+}
+
+int main() {
+    string s = "the sky is blue";
+    string reversed = reverseWords(s);
+    cout << reversed << endl;
+
+    return 0;
+}
