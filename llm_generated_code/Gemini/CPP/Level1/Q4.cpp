8973b01ab79f19bb961a358b7fb890b7d36d45be
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-03 17:23:30 +0530

    Update Q4.cpp

--- llm_generated_code/Gemini/CPP/Level1/Q4.cpp
+++ llm_generated_code/Gemini/CPP/Level1/Q4.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int n = prices.size();
+    if (n <= 1) {
+        return 0;
+    }
+
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < n; ++i) {
+        minPrice = min(minPrice, prices[i]);
+        maxProfit = max(maxProfit, prices[i] - minPrice);
+    }
+
+    return maxProfit;
+}
+
+int main() {
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    int maxProfit = maxProfit(prices);
+    cout << "Maximum profit: " << maxProfit << endl;
+
+    return 0;
+}
