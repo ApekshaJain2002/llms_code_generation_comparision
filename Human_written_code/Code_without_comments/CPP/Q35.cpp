2c9fd527ba6203cdfb41673c7fe1d4d85ec4ffd7
avishagoyal08 <90690345+avishagoyal08@users.noreply.github.com>
2024-11-07 23:35:50 +0530

    Update Q35.cpp

--- Human_written_code/Code_without_comments/CPP/Q35.cpp
+++ Human_written_code/Code_without_comments/CPP/Q35.cpp
@@ -1,31 +1,44 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
-int longestIncreasingSubsequence(int arr[], int n) {
-    vector<int> temp;
-    temp.push_back(arr[0]);
-
-    int len = 1;
-
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > temp.back()) {
-            temp.push_back(arr[i]);
-            len++;
-        } else {
-            int ind = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
-            temp[ind] = arr[i];
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, f, k;
+        cin >> n >> f >> k;
+ 
+        vector<int> cubes(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> cubes[i];
         }
+        int fav = cubes[f-1];
+        sort(cubes.begin(), cubes.end(), greater<int>());
+        bool a=true,b=true;
+        for(int i=0;i<k;i++)
+        {
+            if(cubes[i]==fav)
+            {
+                b=false;
+            }
+        }
+        for(int i=k;i<n;i++)
+        {
+            if(cubes[i]==fav)
+            {
+                a=false;
+            }
+        }
+        if(a==true)
+        cout<<"YES"<<endl;
+        else if(b==true)
+        cout<<"NO"<<endl;
+        else
+        cout<<"MAYBE"<<endl;
+        
     }
-
-    return len;
-}
-
-int main() {
-    int arr[] = {10, 9, 2, 5, 3, 7, 101, 18};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    cout << "The length of the longest increasing subsequence is " << longestIncreasingSubsequence(arr, n);
-
+ 
     return 0;
 }
-
