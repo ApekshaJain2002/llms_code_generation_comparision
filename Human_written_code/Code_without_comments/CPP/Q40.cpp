09ab5db95bc0a9a794884040e4f82c3752632629
avishagoyal08 <90690345+avishagoyal08@users.noreply.github.com>
2024-11-08 15:20:06 +0530

    Update Q40.cpp

--- Human_written_code/Code_without_comments/CPP/Q40.cpp
+++ Human_written_code/Code_without_comments/CPP/Q40.cpp
@@ -1,42 +1,46 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-vector<int> nextGreaterPermutation(vector<int> &A) {
-    int n = A.size();
-    int ind = -1;
+int main()
+{
+	int t;
+	cin >> t;
 
-    for (int i = n - 2; i >= 0; i--) {
-        if (A[i] < A[i + 1]) {
-            ind = i;
-            break;
-        }
-    }
+	while (t--)
+	{
+		int n;
+		cin >> n;
 
-    if (ind == -1) {
-        reverse(A.begin(), A.end());
-        return A;
-    }
+		vector<int> a(n);
+		for (int i = 0; i < n; i++) cin >> a[i];
 
-    for (int i = n - 1; i > ind; i--) {
-        if (A[i] > A[ind]) {
-            swap(A[i], A[ind]);
-            break;
-        }
-    }
+		bool inf = true;
+		int minval = a[0];
 
-    reverse(A.begin() + ind + 1, A.end());
-    return A;
+		for (int i = 1; i < n; i++)
+		{
+			if (a[i] != a[0])
+			{
+				inf = false;
+				break;
+			}
+			minval = min(minval, a[i]);
+		}
+
+		if (inf)
+		{
+			cout << "-1\n";
+			continue;
+		}
+
+		sort(a.begin(), a.end());
+
+		int ans = 0;
+		for (int i = 0; i < n; i++)
+			ans = gcd(ans, a[i] - minval);
+		cout << ans << '\n';
+	}
+
+	return 0;
 }
-
-int main() {
-    vector<int> A = {2, 1, 5, 4, 3, 0, 0};
-    vector<int> ans = nextGreaterPermutation(A);
-
-    cout << "The next permutation is: [";
-    for (auto it : ans) {
-        cout << it << " ";
-    }
-    cout << "]\n";
-    return 0;
-}
-
