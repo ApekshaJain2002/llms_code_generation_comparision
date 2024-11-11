ffcc142c19cbc67897c6f7e203bc257fe55fbce3
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-11 19:57:01 +0530

    Update Q1.cpp

--- llm_generated_code/ChatGPT/CPP/Level1/Q1.cpp
+++ llm_generated_code/ChatGPT/CPP/Level1/Q1.cpp
@@ -5,48 +5,42 @@
 
 using namespace std;
 
-long long maxEarnings(int n, int m, vector<int>& brands, vector<int>& prices) {
-    unordered_map<int, vector<int>> brandPrices;
-    
-    for (int i = 0; i < m; ++i) {
-        brandPrices[brands[i]].push_back(prices[i]);
+long long maxRevenue(int n, vector<int>& brands, vector<int>& prices) {
+    unordered_map<int, long long> brandRevenue;
+    for (int i = 0; i < brands.size(); i++) {
+        brandRevenue[brands[i]] += prices[i];
     }
-    
-    vector<long long> maxBrandEarnings;
-    for (auto& bp : brandPrices) {
-        sort(bp.second.begin(), bp.second.end(), greater<int>());
-        long long sum = 0;
-        for (int price : bp.second) {
-            sum += price;
-        }
-        maxBrandEarnings.push_back(sum);
+
+    vector<long long> revenues;
+    for (const auto& pair : brandRevenue) {
+        revenues.push_back(pair.second);
     }
-    
-    sort(maxBrandEarnings.begin(), maxBrandEarnings.end(), greater<long long>());
+
+    sort(revenues.rbegin(), revenues.rend());
+
     long long maxEarnings = 0;
-    for (int i = 0; i < n && i < maxBrandEarnings.size(); ++i) {
-        maxEarnings += maxBrandEarnings[i];
+    for (int i = 0; i < min(n, (int)revenues.size()); i++) {
+        maxEarnings += revenues[i];
     }
-    
+
     return maxEarnings;
 }
 
 int main() {
     int t;
     cin >> t;
-    
+
     while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector<int> brands(m);
-        vector<int> prices(m);
-        
-        for (int i = 0; i < m; ++i) {
+        int m, n;
+        cin >> m >> n;
+
+        vector<int> brands(m), prices(m);
+        for (int i = 0; i < m; i++) {
             cin >> brands[i] >> prices[i];
         }
-        
-        cout << maxEarnings(n, m, brands, prices) << endl;
+
+        cout << maxRevenue(n, brands, prices) << endl;
     }
-    
+
     return 0;
 }
