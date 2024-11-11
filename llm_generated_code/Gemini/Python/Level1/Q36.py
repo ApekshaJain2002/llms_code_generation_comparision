954ffb10e034867616690adc35b46bb39e4b5d92
avishagoyal08 <90690345+avishagoyal08@users.noreply.github.com>
2024-11-12 01:16:18 +0530

    Create Q36.py

---
+++ llm_generated_code/Gemini/Python/Level1/Q36.py
@@ -0,0 +1,16 @@
+def min_operations(a):
+    n = len(a)
+    if n == 1:
+        return 0
+    
+    freq = {}
+    for num in a:
+        freq[num] = freq.get(num, 0) + 1
+    
+    max_freq = max(freq.values())
+    return n - max_freq
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    print(min_operations(a))
