b2429aca8dec29669ab41cac017c75a5ecb4481f
ApekshaJain2002 <115499283+ApekshaJain2002@users.noreply.github.com>
2024-11-11 20:03:58 +0530

    Update Q1.cpp

--- llm_generated_code/ChatGPT/CPP/Level2/Q1.cpp
+++ llm_generated_code/ChatGPT/CPP/Level2/Q1.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 #include <algorithm>
+
 using namespace std;
 
+long long maxRevenue(int n, vector<int>& brands, vector<int>& prices) {
+    unordered_map<int, long long> brandRevenue;
+    for (int i = 0; i < brands.size(); i++) {
+        brandRevenue[brands[i]] += prices[i];
+    }
+
+    vector<long long> revenues;
+    for (const auto& pair : brandRevenue) {
+        revenues.push_back(pair.second);
+    }
+
+    sort(revenues.rbegin(), revenues.rend());
+
+    long long maxEarnings = 0;
+    for (int i = 0; i < min(n, (int)revenues.size()); i++) {
+        maxEarnings += revenues[i];
+    }
+
+    return maxEarnings;
+}
+
 int main() {
-    int chocolates[] = {5, 2, 12, 20, 1};
-    int maxChocolates = *max_element(chocolates, chocolates + 5);
-    cout << "The highest number of chocolates that one has is: " << maxChocolates << endl;
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int m, n;
+        cin >> m >> n;
+
+        vector<int> brands(m), prices(m);
+        for (int i = 0; i < m; i++) {
+            cin >> brands[i] >> prices[i];
+        }
+
+        cout << maxRevenue(n, brands, prices) << endl;
+    }
+
     return 0;
 }
