8553fb6b6ecb4dc92d00f3c2e67425dc7084f777
Abiha Naqvi <88877269+aBeeHivee@users.noreply.github.com>
2024-11-13 11:12:11 +0530

    Update Q38.py

--- Human_written_code/Code_without_comments/Python/Q38.py
+++ Human_written_code/Code_without_comments/Python/Q38.py
@@ -1,17 +1,27 @@
-from sortedcontainers import SortedList
-def maxSlidingWindow(arr, k):
-    ans = []
-    window = SortedList()
-    for i in range(k):
-        window.add((-arr[i], i))  
-    ans.append(-window[0][0])
-    for i in range(k, len(arr)):
-        window.add((-arr[i], i))
-        if i - k >= 0:
-            window.remove((-arr[i - k], i - k))
-        ans.append(-window[0][0])
-    return ans
-arr = [2, 3, 7, 9, 5, 1, 6, 4, 3]
-k = 3
-result = maxSlidingWindow(arr, k)
-print(result)
+def cmp(a, b):
+    if a[0] != b[0]:
+        return a[0] - b[0]
+    return b[1] - a[1]
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        v = [tuple(map(int, input().split())) for _ in range(n)]
+        v.sort(key=lambda x: (x[0], -x[1]))
+
+        idx, cnt, total = 0, 0, 0
+        for p in range(n):
+            if p < idx:
+                continue
+            total += v[p][1]
+            cnt += 1
+            while idx < n and v[idx][0] <= cnt:
+                idx += 1
+            cnt = p - idx + 1
+            cnt = max(cnt, 0)
+
+        print(total)
+
+if __name__ == "__main__":
+    main()
